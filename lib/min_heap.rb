78628831d2ebde3e4ef613cc6d35bd181fd37440
Sharon Cheung <keikei1128@g.ucla.edu>
2020-09-24 19:43:16 -0700

    finished min_heap

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -17,7 +17,8 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    @store << HeapNode.new(key, value)
+    heap_up(@store.length - 1)
   end
 
   # This method removes and returns an element from the heap
@@ -25,7 +26,14 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+    # min heap always remove the smallest element
+    return nil if @store.empty?
+    # swap it with the last node, so we can just pop off the last node in the heap
+    swap(0, @store.length - 1)
+    result = @store.pop
+    
+    heap_down(0) unless @store.empty?
+    return result.value
   end
 
 
@@ -47,7 +55,7 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    return @store.empty?
   end
 
   private
@@ -55,17 +63,46 @@ class MinHeap
   # This helper method takes an index and
   #  moves it up the heap, if it is less than it's parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(1)
+  # Space complexity: O(log n)
   def heap_up(index)
-    
+    return if index == 0 # root node
+    # compare the new node with its parent
+    # if out of order, swap
+    parent = (index - 1) / 2
+    if @store[index].key < @store[parent].key
+      swap(index, parent)
+      heap_up(parent)
+    end
+    return
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+    # will stop when it reached the leave node
+
+    return if index == @store.length 
+    left_child = 2 * index + 1
+    right_child = 2 * index + 2
+    # compare to the smallest child for min heap
+    if right_child < @store.length
+      if @store[left_child].key > @store[right_child].key
+        smallest_child = right_child
+      else
+        smallest_child = left_child
+      end
+
+      if @store[index].key > @store[smallest_child].key
+        swap(index, smallest_child)
+        heap_down(smallest_child)
+      end
+    elsif left_child < @store.length
+      if @store[index].key > @store[left_child].key
+        swap(index, left_child)
+      end
+    end
   end
 
   # If you want a swap method... you're welcome
